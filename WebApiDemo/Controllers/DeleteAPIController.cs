d3c21fba803777f1e3883f1ce44ef8ba9cff9c70
wanggang <wanggang-bj@fang.com>
2018-06-07 14:37:57 +0800

    delete

---
+++ WebApiDemo/Controllers/DeleteAPIController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Controllers
+{
+    public class DeleteAPIController : ApiController
+    {
+        [HttpDelete]
+        public bool OptDelete(List<Person> lstChargin)
+        {
+            return true;
+        }
+    }
+}
--- WebApiDemo/Controllers/TestController.cs
+++ WebApiDemo/Controllers/TestController.cs
@@ -32,5 +32,10 @@ namespace WebApiDemo.Controllers
         {
             return View();
         }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
     }
 }
